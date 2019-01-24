7309470d78a5435f4e080da9318ec769e1c72ccf
Hiroki.T <hirodora@me.com>
2019-01-25 06:41:08 +0900

    add: get result

--- nodejob.py
+++ nodejob.py
@@ -5,6 +5,30 @@ import types
 import time
 
 
+class NodeJobResult:
+    """Result iterator class"""
+    def __init__(self, non_ordered_list: list):
+        self._list = non_ordered_list
+        self._x = 0
+        self._y = 0
+        self._x_max = len(non_ordered_list)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._x == self._x_max:
+            self._x = 0
+            self._y += 1
+        try:
+            return_val = self._list[self._x][self._y]
+        except IndexError:
+            raise StopIteration()
+
+        self._x += 1
+        return return_val
+
+
 class Master:
     def __init__(self, address_list: list):
         """Initialize method
@@ -24,19 +48,21 @@ class Master:
                        for i in range(clients_count)]
 
         pickled_function = pickle.dumps(function)
-        print(pickled_function)
 
         # Connect to worker
         [client.connect(worker)
          for client, worker in zip(self.clients, self.address_list)]
 
         # Send function-object to worker
+        # map(lambda x: x.sendall(pickled_function), self.clients)
         [client.send(pickled_function) for client in self.clients]
-        # time break
         time.sleep(0.001)
         # Send data blocks to worker
         [client.send(pickle.dumps(data_block))
          for client, data_block in zip(self.clients, data_blocks)]
+
+        returns = [pickle.loads(client.recv(2**30)) for client in self.clients]
+        return NodeJobResult(returns)
 
 
 class Worker():
@@ -52,26 +78,30 @@ class Worker():
 
                 if count == 0:
                     function = pickle.loads(data)
-                    print('function', function)
                     count += 1
                 elif count == 1:
+                    print(data)
                     data_block = pickle.loads(data)
-                    print('data', data_block)
-                    self.__work(function, data_block)
-                    count += 1
+                    returns = self._work(function, data_block)
+                    self.request.send(pickle.dumps(returns))
                 else:
                     break
 
             self.request.close()
-        
-        def __work(self, function: types.FunctionType, data_bloc: list):
+
+        def _work(self, function: types.FunctionType, data_bloc: list):
             """execute function"""
+            returns = []
             for data in data_bloc:
-                function(data)
+                returns.append(function(data))
+
+            return returns
 
     def __init__(self, host: str, port: int):
         self.HOST = host
         self.PORT = port
-        server = socketserver.TCPServer((self.HOST, self.PORT), self.Handler)
-        print('work on', server.socket.getsockname())
-        server.serve_forever()
+        while True:
+            server = socketserver.TCPServer((self.HOST, self.PORT),
+                                            self.Handler)
+            print('work on', server.socket.getsockname())
+            server.serve_forever()
